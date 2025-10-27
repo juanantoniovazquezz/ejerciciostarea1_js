3f02c8d47ac31f2217a44409d08fc67343bd2a5b
juanantoniovazquezz <juatorvazquez@gmail.com>
2025-10-27 12:20:08 +0100

    Update ejercicio8.js

--- TAREA 1.1/ejercicio8.js
+++ TAREA 1.1/ejercicio8.js
@@ -1,24 +1,11 @@
-// ejercicio8.js
-// Programa que indica si un número es par o impar
-// Si el número es 0, indica que no es ni par ni impar
+const prompt = require('prompt-sync')();
 
-const readline = require("readline");
+let numero = parseInt(prompt("Introduce un número entero: "));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("Introduce un número entero: ", (num) => {
-  num = parseInt(num); // Convertimos a entero
-
-  if (num === 0) {
-    console.log("El número no es par ni impar.");
-  } else if (num % 2 === 0) {
-    console.log(`El número ${num} es par.`);
-  } else {
-    console.log(`El número ${num} es impar.`);
-  }
-
-  rl.close();
-});
+if (numero === 0) {
+    console.log("El número no es par ni impar");
+} else if (numero % 2 === 0) {
+    console.log("El número es par");
+} else {
+    console.log("El número es impar");
+}
