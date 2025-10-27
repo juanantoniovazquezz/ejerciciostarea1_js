0726d13c2dcf957e25fcefd6f164a450b5613a91
juanantoniovazquezz <juatorvazquez@gmail.com>
2025-10-27 12:27:12 +0100

    Update ejercicio3.js

--- TAREA 1.1/ejercicio3.js
+++ TAREA 1.1/ejercicio3.js
@@ -1,28 +1,12 @@
-// ejercicio3.js
-// Programa que lee dos números y muestra cuál es mayor o si son iguales
+const prompt = require('prompt-sync')();
 
-const readline = require("readline");
+let num1 = parseFloat(prompt("Introduce el primer número: "));
+let num2 = parseFloat(prompt("Introduce el segundo número: "));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("Introduce el primer número: ", (num1) => {
-  rl.question("Introduce el segundo número: ", (num2) => {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-
-    console.log("\nResultado:");
-
-    if (num1 > num2) {
-      console.log(`El número mayor es: ${num1}`);
-    } else if (num2 > num1) {
-      console.log(`El número mayor es: ${num2}`);
-    } else {
-      console.log("Ambos números son iguales.");
-    }
-
-    rl.close();
-  });
-});
+if (num1 > num2) {
+    console.log(`El mayor es: ${num1}`);
+} else if (num2 > num1) {
+    console.log(`El mayor es: ${num2}`);
+} else {
+    console.log("Ambos números son iguales");
+}
