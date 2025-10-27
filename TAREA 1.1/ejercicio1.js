c1dd823445ac534ee723c58f3b279d93d18dd806
juanantoniovazquezz <juatorvazquez@gmail.com>
2025-10-27 12:28:26 +0100

    Refactor code to remove unnecessary comments

--- TAREA 1.1/ejercicio1.js
+++ TAREA 1.1/ejercicio1.js
@@ -1,16 +1,13 @@
-// Cargar prompt-sync
-const prompt = require('prompt-sync')();
 
-// Pedir al usuario los valores
+const prompt = require('prompt-sync')();
 let A = Number(prompt('Introduce el valor de A: '));
 let B = Number(prompt('Introduce el valor de B: '));
 
 console.log(`\nValores iniciales: A = ${A}, B = ${B}`);
-
-// Intercambiar valores
 [A, B] = [B, A]; // Forma moderna sin variable temporal
 
 console.log(`Valores intercambiados: A = ${A}, B = ${B}`);
+
 
 
 
